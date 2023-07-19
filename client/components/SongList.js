402bfee649aa683b298b4502b97d76f5ed6d8bb3
Nataly Galushka <37546130+hellared@users.noreply.github.com>
2023-07-19 12:58:12 +0300

    Add mutations and queries

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,9 +1,45 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { Link } from "react-router";
+import query from "../queries/fetchSongs";
+import mutation from "../mutations/deleteSong";
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props
+      .mutate({
+        variables: {
+          id,
+        },
+      })
+      .then(() => this.props.data.refetch());
+  }
+
+  renderSongs() {
+    return this.props.data.songs.map(({ id, title }) => {
+      return (
+        <li key={id} className="collection-item">
+          {title}
+          <i className="material-icons" onClick={() => this.onSongDelete(id)}>
+            delete
+          </i>
+        </li>
+      );
+    });
+  }
   render() {
-    return <div>SongList</div>;
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <ul className="collection">{this.renderSongs()}</ul>
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    );
   }
 }
 
-export default SongList;
+export default graphql(mutation)(graphql(query)(SongList));
